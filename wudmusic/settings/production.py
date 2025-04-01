95aa7a101f24b076b8f2eb5dc7dc473d1513f4dc
xpoes123 <logiccubing@gmail.com>
2025-04-01 17:06:27 -0500

    added custom urls

--- wudmusic/settings/production.py
+++ wudmusic/settings/production.py
@@ -4,7 +4,7 @@ import django_heroku
 
 DEBUG = True
 SECRET_KEY = os.getenv('SECRET_KEY')
-ALLOWED_HOSTS = ['wudmusic.herokuapp.com', 'wudmusic-bee92661c8ff.herokuapp.com']
+ALLOWED_HOSTS = ['wudmusic.herokuapp.com', 'wudmusic-bee92661c8ff.herokuapp.com', 'wudmusic.com', 'www.wudmusic.com',]
 
 DATABASES = {
     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
